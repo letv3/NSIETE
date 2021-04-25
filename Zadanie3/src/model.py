06b65895e51d9008d2f017a3654cf5701c326606
Andrej Petričko <32256397+xPetricko@users.noreply.github.com>
2021-04-25 21:04:52 +0200

    Update model.py

--- Zadanie3/src/model.py
+++ Zadanie3/src/model.py
@@ -7,7 +7,10 @@ import torch.optim as optim
 from torch.distributions import Beta
 from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 
-from src.net import Net
+from src.Net import Net
+
+from src.Net import Actor, Critic
+from gym import spaces
 
 
 class Agent:
@@ -16,13 +19,74 @@ class Agent:
     ppo_epoch = 10
     buffer_capacity, batch_size = 2000, 128
 
-    def __init__(self, alpha, gamma, img_stack):
+    def __init__(self, alpha, gamma, env):
         self.alpha = alpha
         self.gamma = gamma
-        self.img_stack = img_stack
+        self.action_space = env.action_space
+        self.observation_space = env.observation_space
 
-        self.transition = np.dtype([('s', np.float64, (self.img_stack, 96, 96)), ('a', np.float64, (3,)), ('a_logp', np.float64),
-                                    ('r', np.float64), ('s_n', np.float64, (self.img_stack, 96, 96))])
+        self.training_step = 0
+        self.device = T.device("cuda" if T.cuda.is_available() else "cpu")
+
+        self.actor = Actor(
+            alpha=self.alpha,
+            obs_space=self.observation_space.shape[0],
+            action_space=self.action_space.n if hasattr(self.action_space, "n") else self.action_space.shape[0]
+        ).double().to(self.device)
+
+        self.critic = Critic(
+            alpha=self.alpha,
+            obs_space=self.observation_space.shape[0],
+            action_space=self.action_space.n if hasattr(self.action_space, "n") else self.action_space.shape[0]
+        ).double().to(self.device)
+
+        self.transition = np.dtype(
+            [('s', np.float64, self.observation_space.shape), ('a', np.float64, self.action_space.shape),
+             ('a_logp', np.float64),
+             ('r', np.float64), ('s_n', np.float64, self.observation_space.shape)])
+
+        self.buffer = np.empty(self.buffer_capacity, dtype=self.transition)
+        self.counter = 0
+
+    def select_action(self, state):
+        state_x = T.from_numpy(state).float().to(self.device)
+        action = self.actor.forward(state_x).detach().numpy()
+
+        if type(self.action_space) == spaces.Box:
+            action = action  # TODO Implement action handling if continuous
+        elif type(self.action_space) == spaces.Discrete:
+            action = action  # TODO Implement action handling if discrete
+        else:
+            raise Exception("Invalid action space type.")
+        return action
+        # return action.clip(min=self.action_space.low, max=self.action_space.high)  # TODO: clip action?
+
+    def store(self, transition):
+        self.buffer[self.counter] = transition
+        self.counter += 1
+        if self.counter == self.buffer_capacity:
+            self.counter = 0
+            return True
+        else:
+            return False
+
+    def update(self):  # TODO Implement update
+        pass
+
+
+class Agent_old:
+    max_grad_norm = 0.5
+    clip_param = 0.1
+    ppo_epoch = 10
+    buffer_capacity, batch_size = 2000, 128
+
+    def __init__(self, alpha, gamma):
+        self.alpha = alpha
+        self.gamma = gamma
+
+        self.transition = np.dtype(
+            [('s', np.float64, (self.img_stack, 96, 96)), ('a', np.float64, (3,)), ('a_logp', np.float64),
+             ('r', np.float64), ('s_n', np.float64, (self.img_stack, 96, 96))])
         self.training_step = 0
         self.device = T.device("cuda" if T.cuda.is_available() else "cpu")
 
@@ -45,7 +109,7 @@ class Agent:
         return action, a_logp
 
     def save_param(self, name):
-        T.save(self.net.state_dict(), 'data/model/'+name+'.pkl')
+        T.save(self.net.state_dict(), 'data/model/' + name + '.pkl')
 
     def store(self, transition):
         self.buffer[self.counter] = transition
@@ -57,7 +121,7 @@ class Agent:
             return False
 
     def load_param(self, name):
-        self.net.load_state_dict(T.load('data/model/'+name+'.pkl'))
+        self.net.load_state_dict(T.load('data/model/' + name + '.pkl'))
 
     def update(self):
         self.training_step += 1
@@ -77,7 +141,6 @@ class Agent:
 
         for _ in range(self.ppo_epoch):
             for index in BatchSampler(SubsetRandomSampler(range(self.buffer_capacity)), self.batch_size, False):
-
                 alpha, beta = self.net(s[index])[0]
                 dist = Beta(alpha, beta)
                 a_logp = dist.log_prob(a[index]).sum(dim=1, keepdim=True)
