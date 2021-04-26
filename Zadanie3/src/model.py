e9f0e214e2f87e848e97e1fcf25436779eb8ecbe
Andrej Petričko <32256397+xPetricko@users.noreply.github.com>
2021-04-26 15:09:29 +0200

    Update model.py

--- Zadanie3/src/model.py
+++ Zadanie3/src/model.py
@@ -10,6 +10,8 @@ from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 from src.Net import Net
 
 from src.Net import Actor, Critic
+from src.Memory import Memory
+
 from gym import spaces
 
 
@@ -17,61 +19,103 @@ class Agent:
     max_grad_norm = 0.5
     clip_param = 0.1
     ppo_epoch = 10
-    buffer_capacity, batch_size = 2000, 128
+    buffer_capacity, batch_size = 5000, 512
 
-    def __init__(self, alpha, gamma, env):
+    def __init__(self,env,alpha=1e-4, gamma=0.99, tau=1e-2):
+
         self.alpha = alpha
         self.gamma = gamma
+        self.tau = tau
+
         self.action_space = env.action_space
         self.observation_space = env.observation_space
 
         self.training_step = 0
         self.device = T.device("cuda" if T.cuda.is_available() else "cpu")
 
+        # Actor-Critic initialization
         self.actor = Actor(
-            alpha=self.alpha,
+            obs_space=self.observation_space.shape[0],
+            action_space=self.action_space.n if hasattr(self.action_space, "n") else self.action_space.shape[0]
+        ).double().to(self.device)
+
+        self.actor_target = Actor(
             obs_space=self.observation_space.shape[0],
             action_space=self.action_space.n if hasattr(self.action_space, "n") else self.action_space.shape[0]
         ).double().to(self.device)
 
         self.critic = Critic(
-            alpha=self.alpha,
             obs_space=self.observation_space.shape[0],
             action_space=self.action_space.n if hasattr(self.action_space, "n") else self.action_space.shape[0]
         ).double().to(self.device)
 
+        self.critic_target = Critic(
+            obs_space=self.observation_space.shape[0],
+            action_space=self.action_space.n if hasattr(self.action_space, "n") else self.action_space.shape[0]
+        ).double().to(self.device)
+
+        # Duplicate networks
+        for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
+            target_param.data.copy_(param.data)
+        for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
+            target_param.data.copy_(param.data)
+
+        # Transition memory initialization
         self.transition = np.dtype(
             [('s', np.float64, self.observation_space.shape), ('a', np.float64, self.action_space.shape),
-             ('a_logp', np.float64),
-             ('r', np.float64), ('s_n', np.float64, self.observation_space.shape)])
+             ('r', np.float64), ('s_n', np.float64, self.observation_space.shape), ("d", np.float64, (1,))])
+        self.memory = Memory(self.buffer_capacity, self.transition)
 
-        self.buffer = np.empty(self.buffer_capacity, dtype=self.transition)
-        self.counter = 0
+        # Training
+        self.critic_criterion = nn.MSELoss()
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=alpha)
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=alpha)
 
     def select_action(self, state):
-        state_x = T.from_numpy(state).float().to(self.device)
-        action = self.actor.forward(state_x).detach().numpy()
+        state_x = T.from_numpy(state).to(self.device)
+        action = self.actor.forward(state_x)
 
         if type(self.action_space) == spaces.Box:
-            action = action  # TODO Implement action handling if continuous
+            action = T.tanh(action).detach().numpy()
         elif type(self.action_space) == spaces.Discrete:
-            action = action  # TODO Implement action handling if discrete
+            action = action  # TODO Implement action handling if disccrete
         else:
             raise Exception("Invalid action space type.")
         return action
         # return action.clip(min=self.action_space.low, max=self.action_space.high)  # TODO: clip action?
 
-    def store(self, transition):
-        self.buffer[self.counter] = transition
-        self.counter += 1
-        if self.counter == self.buffer_capacity:
-            self.counter = 0
-            return True
-        else:
-            return False
-
     def update(self):  # TODO Implement update
-        pass
+        states, actions, rewards, next_states, _ = self.memory.sample(self.batch_size)
+        states = T.DoubleTensor(states)
+        actions = T.DoubleTensor(actions)
+        rewards = T.DoubleTensor(rewards)
+        next_states = T.DoubleTensor(next_states)
+
+        # Critic loss
+        Qvals = self.critic.forward(states, actions)
+        next_actions = self.actor_target.forward(next_states)
+        next_Q = self.critic_target.forward(next_states, next_actions.detach())
+        Qprime = rewards + self.gamma * next_Q
+        critic_loss = self.critic_criterion(Qvals, Qprime)
+
+        # Actor loss
+        policy_loss = -self.critic.forward(states, self.actor.forward(states)).mean()
+
+        # update networks
+        self.actor_optimizer.zero_grad()
+        policy_loss.backward()
+        self.actor_optimizer.step()
+
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        # update target networks
+        for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
+            target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
+
+        for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
+            target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
 
 
 class Agent_old:
