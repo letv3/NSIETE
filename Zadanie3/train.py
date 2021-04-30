c429e017f253ae1957286e28ad8ac42d422af043
letv3 <xlytvyn@stuba.sk>
2021-04-30 15:05:41 +0200

    other solution
    works for now but need refactoring and cleaning

--- Zadanie3/train.py
+++ Zadanie3/train.py
@@ -3,29 +3,25 @@ import torch
 from src.Env import Env
 from src.Model import Agent
 import wandb
-NUM_EPISODES = 8000  # Number of episodes
+NUM_EPISODES = 6000  # Number of episodes
 MAX_STEPS = 2000  # Max number of steps in one episodes
 EARLY_STOP_EPISODES = 10  # Stop after n episodes no progress
 LOG_INTERVAL = 10  # Log after n of episodes
-RENDER = True
+RENDER = False
 
 # Torch setup
 torch.manual_seed(0)
 if torch.cuda.is_available():
     device = torch.device('cuda')
-    torch.cuda.manual_seed(0)
+
 
 # Main program
 if __name__ == "__main__":
 
-    learning_rate = 1e-4
-    gamma = 0.99
-    tau = 1e-2
-
     # Wandb init
     # run = wandb.init(project='', entity='xpetricko', monitor_gym=True)
-    run = wandb.init(project='zadani3-lunarlander', entity='lytyvnol', monitor_gym=True)
-    run.name = f"Test-run-{NUM_EPISODES}"
+    # run = wandb.init(project='zadani3-lunarlander', entity='lytyvnol', monitor_gym=True)
+    # run.name = f"Test-run-{NUM_EPISODES}"
 
     env = Env()
     agent = Agent(env.env)
@@ -34,21 +30,21 @@ if __name__ == "__main__":
     moving_reward_history = []
     max_reward = 0
     average_reward = 0
+    average_moving_reward = 0
     episodes_with_lower_score = 0
 
     # Wandb config
-    config = wandb.config
-    config.learing_rate = learning_rate
-    config.gamma = gamma
-    config.tau = tau
-    config.clip_parameter = agent.clip_param
-    config.buffer_capacity = agent.buffer_capacity
-    config.batch_size = agent.batch_size
+    # config = wandb.config
+    # config.learing_rate = agent.alpha
+    # config.gamma = agent.gamma
+    # config.tau = agent.tau
+    # config.clip_parameter = agent.clip_param
+    # config.buffer_capacity = agent.buffer_capacity
+    # config.batch_size = agent.batch_size
 
     for episode in range(NUM_EPISODES):
         moving_reward = 0
         state = np.float64(env.reset())
-        # state = torch.from_numpy(state).to(device)
 
         # Actions in one episode
         for t in range(MAX_STEPS):
@@ -80,24 +76,23 @@ if __name__ == "__main__":
             print('Ep {}\tLast score: {:.2f}\tMoving average score: {:.2f}'.format(
                  episode, reward, average_reward))
 
-        wandb.log({
-            'Episode': episode,
-            'Step': t,
-            'Score': reward,
-            'Average Score': average_reward
-
-        })
         # wandb.log({
-        #     'Environment': [wandb.Image(env.render(mode='rgb_array'), caption='Environment')]
+        #     'Episode': episode,
+        #     'Step': t,
+        #     'Score': moving_reward,
+        #     'Average Score': average_reward
+        #     # 'Environment': [wandb.Image(env.render(mode='rgb_array'), caption='Environment')]
+        # })
+        # wandb.log({
+        #
         # })  # https://docs.wandb.ai/guides/track/log#images-and-overlays
         # Nemusime asi tak to manualne robit, ked ze wandb priamo podporuje GYM toolkit
         # monitor_gym=True
 
     torch.cuda.empty_cache()
-    run.finish()
+    # run.finish()
 
-            print('Ep {}\tLast score: {:.2f}\t Average score: {:.2f}\t Moving average score: {:.2f}'.format(
-                 episode, reward, average_reward,moving_reward))
+
         #
         #     if args.save:
         #         agent.save_param(name=str(args.train_ord) + "_model_state")
